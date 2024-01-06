b6f3d66d12036728a244ba0f6cea79f24dfac61d
Zaki <menza.zm@gmail.com>
2024-01-06 15:56:30 +0600

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Zaki on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
